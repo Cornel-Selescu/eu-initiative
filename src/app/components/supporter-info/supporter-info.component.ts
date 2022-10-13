5d8c7dfeed194e6783f1395203c17c94b0c778e3
cornelsel <cornelselescu@gmail.com>
2022-10-13 21:19:00 +0300

    Form validation added.

--- src/app/components/supporter-info/supporter-info.component.ts
+++ src/app/components/supporter-info/supporter-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter , OnInit} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { CustomValidators } from 'src/app/validators/custom-validators';
 
@@ -9,7 +9,7 @@ import { Country } from 'src/app/typings/country';
   templateUrl: './supporter-info.component.html',
   styleUrls: ['./supporter-info.component.scss']
 })
-export class SupporterInfoComponent  implements OnInit {
+export class SupporterInfoComponent implements OnInit {
   @Input('initiative-id') initiativeId: string | number;
   @Input('country') country: Country;
 
@@ -19,7 +19,7 @@ export class SupporterInfoComponent  implements OnInit {
   selectableYears: number[] = Array(this.currentYear - 1900).fill(this.currentYear).map((x, i) => x - i);
   monthsInOneYear: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   daysInOneMonth: number[] = Array(31).fill(1).map((x, i) => i + 1);
-  
+
   initiativeForm: FormGroup;
 
   constructor(private fb: FormBuilder) { }
@@ -27,43 +27,84 @@ export class SupporterInfoComponent  implements OnInit {
   ngOnInit(): void {
     const fb = this.fb;
     this.initiativeForm = fb.group({
-      legalDocument: [this.country?.documentOptions[0]?.code || '', [
+      legalDocument: [this.country?.documentOptions[0]?.code || '',
+      [Validators.required]],
+      initiativeId: [this.initiativeId],
+      documentId: ['',
+        [Validators.required],
+        []],
+      firstName: ['', [
         Validators.required,
-        // Validators.minLength(3),
-        // CustomValidators.noSpaces,
-        // Validators.maxLength(10)
-      ], [
-        // CustomValidators.noSpaces
+        Validators.minLength(2),
+        Validators.maxLength(20)
       ]],
-      initiativeId: [this.initiativeId, [Validators.required]],
-      documentId: ['', [Validators.required]],
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      certyfy: [!!this.country.hasEID, [Validators.required]],
-      statement: [!!this.country.hasEID , [Validators.required]],
+      lastName: ['', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(20)
+      ]],
+      certify: [!!this.country.hasEID, [Validators.requiredTrue]],
+      statement: [!!this.country.hasEID, [Validators.requiredTrue]],
       dateOfBirth: fb.group({
-        day: [Validators.required],
-        month: [Validators.required],
-        year: [Validators.required]
+        day: ['', [Validators.required]],
+        month: ['', [Validators.required]],
+        year: ['', [Validators.required]]
       }),
     });
   }
+
+  get legalDocument() {
+    return this.initiativeForm.get('legalDocument');
+  }
+
+  get documentId() {
+    return this.initiativeForm.get('documentId');
+  }
+
+  get firstName() {
+    return this.initiativeForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.initiativeForm.get('lastName');
+  }
+
+  get certify() {
+    return this.initiativeForm.get('certify');
+  }
+
+  get statement() {
+    return this.initiativeForm.get('statement');
+  }
+
+  get day() {
+    return this.initiativeForm.get('dateOfBirth.day');
+  }
+
+  get month() {
+    return this.initiativeForm.get('dateOfBirth.month');
+  }
+
+  get year() {
+    return this.initiativeForm.get('dateOfBirth.year');
+  }
+
+
+
+
+
+
+
+
+
 
   onBack() {
     this.back.emit();
   }
 
   onSubmit() {
-    if (false) { //adds errors
-      this.initiativeForm.setErrors({
-        invalidInitiative: true
-      });
-    } else { // removes the errors
-      this.initiativeForm.setErrors({
-        invalidInitiative: null
-      });
-      this.initiativeForm.updateValueAndValidity();
-      console.log(this.initiativeForm.value);
+    if (this.initiativeForm.valid) { 
+      // Send to the server
     }
   }
 }
